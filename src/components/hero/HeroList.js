7ae95b9d764db265df5a4d9ef83f67bfda657b8d
claudia <claudianataliurena@gmail.com>
2024-05-08 22:07:54 -0300

    añado fotos

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
   return (
-    <>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>{hero.superhero}</li>
-        ))}
-      </ul>
-    </>
+    <div className="row rows-cols-1 row-cols-md-3 g3">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   );
 };
