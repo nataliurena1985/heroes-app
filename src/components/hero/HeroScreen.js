7b5461c8ae6adf5489aa3df7a680a90f18348aa1
claudia <claudianataliurena@gmail.com>
2024-05-09 11:21:49 -0300

    navegacion  con el boton a una pantalla anterior

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const navigate = useNavigate();
+  const { heroeId } = useParams();
+  const hero = getHeroById(heroeId);
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-      <h1>HeroScreen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath} alt={superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b>
+            {alter_ego}
+          </li>
+
+          <li className="list-group-item">
+            <b>Publisher:</b>
+            {publisher}
+          </li>
+
+          <li className="list-group-item">
+            <b>First Appearance:</b>
+            {first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Regresar
+        </button>
+      </div>
     </div>
   );
-}
+};
