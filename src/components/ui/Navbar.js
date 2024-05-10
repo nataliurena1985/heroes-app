fcb74a470ddfd8cb744d98c671139848f4edefe8
claudia <claudianataliurena@gmail.com>
2024-05-09 23:59:02 -0300

    cambie mi nombre

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -44,7 +44,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Fernando</span>
+          <span className="nav-item nav-link text-info">Claudia Ureña</span>
           <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
           </button>
